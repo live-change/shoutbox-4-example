c126d8cd4a2625cb8ba74f814961fd17ff7c7644
Michał Łaszczewski <michal@laszczewski.com>
2021-08-20 04:46:03 +0200

    dependencies updated

--- front/vite.config.js
+++ front/vite.config.js
@@ -49,6 +49,8 @@ module.exports = {
 
   resolve: {
     alias: [
+      { find: 'debug', replacement: 'debug/src/browser.js' },
+      { find: 'universal-websocket-client', replacement: 'universal-websocket-client/browser.js' },
       { find: 'sockjs-client', replacement: 'sockjs-client/dist/sockjs.min.js' }
       //{ find: '@live-change/framework-vue', replacement: path.resolve('../../framework-vue') },
       //{ find: '@live-change/framework-vue-api', replacement: path.resolve('../../framework-vue-api') },
